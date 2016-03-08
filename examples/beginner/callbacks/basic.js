0dbc21cc51403d4751ea4734a90f57c3a1c23ad5
ross <rosshinkley@gmail.com>
2016-03-07 21:52:15 -0500

    added comments to all of the example files

--- examples/beginner/callbacks/basic.js
+++ examples/beginner/callbacks/basic.js
@@ -7,7 +7,7 @@ nightmare
   //load a url
   .goto('http://yahoo.com')
   //simulate typing into an element identified by a CSS selector
-  //here, Nightmare is tying into the search bar
+  //here, Nightmare is typing into the search bar
   .type('input[title="Search"]', 'github nightmare')
   //click an element identified by a CSS selector
   //in this case, click the search button
@@ -16,7 +16,7 @@ nightmare
   //in this case, the body of the results
   .wait('#main')
   //execute javascript on the page
-  //here, the function is getting the first search result
+  //here, the function is getting the HREF of the first search result
   .evaluate(function() {
     return document.querySelector('#main .searchCenterMiddle li a')
       .href;
--- examples/beginner/vo/basic.js
+++ examples/beginner/vo/basic.js
@@ -4,22 +4,35 @@ var Nightmare = require('nightmare'),
     show: true
   });
 
-var run = function*(){
+var run = function*() {
+  //declare the result and wait for Nightmare's queue defined by the following chain of actions to complete
   var result = yield nightmare
-  .goto('http://yahoo.com')
-  .type('input[title="Search"]', 'github nightmare')
-  .click('#uh-search-button')
-  .wait('#main')
-  .evaluate(function() {
-    return document.querySelector('#main .searchCenterMiddle li a')
-      .href;
-  });
+    //load a url
+    .goto('http://yahoo.com')
+    //simulate typing into an element identified by a CSS selector
+    //here, Nightmare is typing into the search bar
+    .type('input[title="Search"]', 'github nightmare')
+    //click an element identified by a CSS selector
+    //in this case, click the search button
+    .click('#uh-search-button')
+    //wait for an element identified by a CSS selector
+    //in this case, the body of the results
+    .wait('#main')
+    //execute javascript on the page
+    //here, the function is getting the HREF of the first search result
+    .evaluate(function() {
+      return document.querySelector('#main .searchCenterMiddle li a')
+        .href;
+    });
 
+  //queue and end the Nightmare instance along with the Electron instance it wraps
   yield nightmare.end();
 
+  //return the HREF
   return result;
 };
 
-vo(run)(function(err, result){
+//use `vo` to execute the generator function
+vo(run)(function(err, result) {
   console.log(result);
 })
